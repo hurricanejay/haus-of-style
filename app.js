13b8578a65816c92dfa1553175fc27f716a66c8b
Jay Wen <jaywen89@gmail.com>
2020-06-08 12:45:41 -0400

    adds initial js variables

---
+++ app.js
@@ -0,0 +1,26 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCart = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// main cart items
+let cart = [];
+
+// getting the products
+class Products {}
+
+//display products
+class UI {}
+
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
